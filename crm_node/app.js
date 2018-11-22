351dd8089ec5d21dbd95c05ef6f67f9cdb142961
yarong.sun@oriente.com <yarong.sun@oriente.com>
2018-11-22 09:56:12 +0800

    update library list

--- crm_node/app.js
+++ crm_node/app.js
@@ -18,6 +18,7 @@ var app = express();
 const HOST = 'http://ec2-52-80-209-215.cn-north-1.compute.amazonaws.com.cn:8082'
 const BizHOST = 'http://ec2-52-80-209-215.cn-north-1.compute.amazonaws.com.cn:8090'
 const CRMHOST = 'http://ec2-52-80-209-215.cn-north-1.compute.amazonaws.com.cn:8030'
+// const CRMHOST = 'http://192.168.12.39:8030'
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -31,6 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     resave: true,
+    rolling:true,
     saveUninitialized: false,
     secret: 'secret',
     cookie: {maxAge: 900000 },  //设置maxAge是900000ms，即15mins后session和相应的cookie失效过期
@@ -78,15 +80,17 @@ app.post('/login',(req, res) => {
         
       }
     } else {
-      res.json({code:-1,message: 'fail' })
+      res.json({code:-1,message: 'User information has expired, please login again!' })
     }
     })
 })
 
 app.get('/list',(req,res,next) => {
+  console.log(8,req.session.sign)
   if(req.session.sign) {
     sign = req.session.sign
     let date = moment(new Date).format('YYYY-MM-DD HH:mm:ss')
+    console.log(9, date)
     let url = '/api/menu/list'
     let text = hmacSHA1(sign.token,sign.tokenSecret,date,'GET', url)
     request.get({
@@ -94,7 +98,8 @@ app.get('/list',(req,res,next) => {
       headers: {
         'PROJECT-NAME': 'CRM',
         'X-Oriente-Date': date,
-        'Authorization': text
+        'Authorization': text,
+        'USERID': 1
       }
     }, (err, resp, data) => {
       if(resp && resp.body) {
@@ -106,7 +111,7 @@ app.get('/list',(req,res,next) => {
           res.json({code: code, message: body.msg })
         }
       } else {
-        res.json({code: -1, message: 'fail'})
+        res.json({code: -1, message: 'User information has expired, please login again!'})
       }
     })
   }
@@ -126,7 +131,7 @@ app.get('/webapi/*', (req, res, next) => {
       }
     })
   } else {
-    res.json({code: -1, msg: 'fail'})
+    res.json({code: -1, msg: 'User information has expired, please login again!'})
   }
 })
 
@@ -146,7 +151,7 @@ app.post('/webapi/*',(req, res, next) => {
       }
     })
   } else {
-    res.json({code: -1, msg: 'fail'})
+    res.json({code: -1, msg: 'User information has expired, please login again!'})
   }
 })
 app.delete('/webapi/*',(req,res,next) => {
@@ -164,13 +169,22 @@ app.delete('/webapi/*',(req,res,next) => {
       }
     })
   } else {
-    res.json({code: -1, msg: 'fail'})
+    res.json({code: -1, msg: 'User information has expired, please login again!'})
   }
 })
 app.get('/api/*',(req,res,next) => {
   if(req.session.sign) {
+    let date = moment(new Date).format('YYYY-MM-DD HH:mm:ss')
+    let url = req.originalUrl
+    let text = hmacSHA1(sign.token,sign.tokenSecret,date,'POST', url)
     request.get({
       url:`${CRMHOST}/${req.originalUrl}`,
+      headers: {
+        'PROJECT-NAME': 'CRM',
+        'X-Oriente-Date': date,
+        'Authorization': text,
+        'USERID': 1
+      }
     },(err,resp,data) => {
       const body = JSON.parse(resp.body)
       const code = body.code
@@ -179,7 +193,54 @@ app.get('/api/*',(req,res,next) => {
       res.json({code:code,msg:msg,data:result})
     })
   } else {
-    res.json({code:-1,msg:'fail'})
+    res.json({code:-1,msg:'User information has expired, please login again!'})
+  }
+})
+app.post('/api/*',(req, res, next) => {
+  if(req.session.sign) {
+    let date = moment(new Date).format('YYYY-MM-DD HH:mm:ss')
+    let url = req.originalUrl
+    let text = hmacSHA1(sign.token,sign.tokenSecret,date,'POST', url)
+    request.post({
+      url:`${CRMHOST}/${req.originalUrl}`,
+      form: req.body,
+      headers: {
+        'PROJECT-NAME': 'CRM',
+        'X-Oriente-Date': date,
+        'Authorization': text,
+        'USERID': 1
+      }
+    },(err, resp,data) => {
+      const body = JSON.parse(resp.body)
+      const code = body.code
+      res.json({ code:code ,msg: body.msg, data: body.result})
+    })
+  } else {
+    res.json({code: -1, msg: 'User information has expired, please login again!'})
+  }
+})
+app.put('/api/*',(req,res,next) => {
+  if(req.session.sign) {
+    let date = moment(new Date).format('YYYY-MM-DD HH:mm:ss')
+    let url = req.originalUrl
+    let text = hmacSHA1(sign.token,sign.tokenSecret,date,'PUT', url)
+    console.log(7,req.originalUrl)
+    request.put({
+      url:`${CRMHOST}/${req.originalUrl}`,
+      headers: {
+        'PROJECT-NAME': 'CRM',
+        'X-Oriente-Date': date,
+        'Authorization': text,
+        'USERID': 1
+      }
+    },(err, resp,data) => {
+      const body = JSON.parse(resp.body)
+      console.log(8,body)
+      const code = body.code
+      res.json({ code:code ,msg: body.msg, data: body.result})
+    })
+  } else {
+    res.json({code: -1, msg: 'User information has expired, please login again!'})
   }
 })
 // catch 404 and forward to error handler
